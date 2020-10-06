f5ac65381e95d33f939269a45981e4dc02435702
Tushar Garg <garg31@uwindsor.ca>
2020-10-06 01:31:47 -0400

    Working to implement chart

--- README.md
+++ README.md
@@ -35,3 +35,59 @@ See the section about [running tests](https://facebook.github.io/create-react-ap
 Designed a basic wireframe using a free tool [draw.io](https://app.diagrams.net/)
 
 <img src="https://github.com/Tushar9721/covid-19-tracker/blob/master/images/Wireframe.png" align="left" height="300px" width="500px" >
+
+### Create Wireframes
+
+All that's needed to install npx is to use npm to install it globally.
+
+```
+npm install npx
+```
+
+> Note: npx installing
+
+To create a new React app just need to write
+
+```
+npx create-react-app app-name
+```
+
+> Note: It can take upto 2 mins to create a react app
+
+### Setting up the environment
+
+Removing unwanted code from the react app
+
+### Structure of COVID Tracker
+
+> Note: Dividing the covid tracker app in different components
+
+1. Header
+2. Title + Select Input drop down feild
+3. Info Box 1
+4. Info Box 2
+5. Info Box 3
+6. Table of Countries
+7. Graph
+8. Map
+
+### Create a Header
+
+> Note: In the header seaction we have displayed our project name and a drop down selector to implement the drop down I have used [Material UI](https://material-ui.com/)
+
+### Create boxes to show cases
+
+> Note: Data is fetched from api provided by [Diesease](https://disease.sh/docs/)
+> 3 Information boxes were created to display the country covid data:
+
+1. Coronavirus Today Cases
+2. Totoal Recoverd
+3. Total Deaths
+
+### Create a country table
+
+A table is built to show the COVID-19 cases of Every country. The first country has the most cases in the world and last country has the least cases.
+
+### create a chart
+
+Using 3-rd party [charts]() to implement the chart, Line graph is implemented
--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import InfoBox from "./InfoBox.js";
+import Map from "./Map.js";
+import Table from "./Table";
+import LineGraph from "./LineGraph";
+import { sortData } from "./util";
+import "./App.css";
 
 function App() {
+  // Creating a useState for countries
+  const [countries, setCountries] = useState([]);
+  //keeping record of selected country
+  const [country, setCountry] = useState("worldwide");
+  const [countryInfo, setCountryInfo] = useState({});
+
+  //setting up the table data
+  const [tableData, setTableData] = useState([]);
+
+  //this is used to set for the worldwide when no option is selected
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
+  //UseEffect is used to load that amount of data from one to n number of times depends of the value given in the array
+  //async -> send a request, wait for it and perfrom some actions
+  //getting the data from disease.sh api here we are getting the country name and code
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, //here we are getting country name
+            value: country.countryInfo.iso2, //here we can fetch country code
+            flag: country.countryInfo.flag,
+          }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  //When ever the drop down is triggered it is called:
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    //fetching data from api
+    const url =
+      countryCode === "worldWide"
+        ? "https://disease.sh/v3/covid-19/all"
+        : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        //specific country code
+        setCountry(countryCode);
+
+        setCountryInfo(data);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div class="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          {/* Helps to implement the country drop down list */}
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {/* These all will be displayed as a selecting items */}
+              {/* Loop through all countries and show a drop down list of the options */}
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          {/* Info Box 1 */}
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          {/* Info Box 2 */}
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          {/* Info Box 3 */}
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
+        </div>
+
+        {/* Map */}
+        <Map />
+      </div>
+
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          {/* Table */}
+          <Table countries={tableData} />
+          <h3>Worldwide New Cases</h3>
+          {/* Graph */}
+          <LineGraph />
+        </CardContent>
+      </Card>
     </div>
   );
 }
